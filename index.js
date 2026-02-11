63fde978f363bf5620410cf6c0ddcfb14668719b
murtaza-5152 <zkr.murtaza@gmail.com>
2026-02-11 17:55:09 +0530

    Feature: Added movie model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import env from "dotenv";
 import mongoose from "mongoose";
+import { Movie } from "./models/movie.model.js";
 
 const app = express(); // app is an express object.
 env.config();
@@ -18,6 +19,16 @@ app.listen(process.env.PORT, async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
     console.log("Database Connected Successfully");
+    await Movie.create({
+      name: "Malamal Weekly",
+      description: "Comedy Action",
+      casts: ["Salman khan", "Anuskha Sharma", "Preeti Jinta"],
+      trailerUrl: "https://www.google.com",
+      language: "Hindi",
+      releaseData: "02/10/1994",
+      director: "Arbaaz Khan",
+      releaseStatus: "RELEASED",
+    });
   } catch (err) {
     console.log("Not be able to connect to Database : ", err);
   }
---
+++ models/movie.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+// Define the schema of movie resource to be stored in DB.
+
+const movieSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    casts: {
+      type: [String],
+      required: true,
+    },
+    trailerUrl: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+      default: "English",
+    },
+    releaseData: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    releaseStatus: {
+      type: String,
+      required: true,
+      default: "RELEASED",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Movie = mongoose.model("Movie", movieSchema);
